ce855bd6a721f5a896499c7180ac97fe3553e107
Yaroslav Nedohoda <y.nedogoda@gmail.com>
2022-11-26 00:25:45 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+let timerId = null;
+stopBtn.setAttribute('disabled', true);
+
+function onStartClick() {
+	timerId = setInterval(() => {
+		document.body.style.backgroundColor = getRandomHexColor();
+	}, 1000);
+	startBtn.setAttribute('disabled', true);
+	stopBtn.removeAttribute('disabled');
+}
+
+function onStopClick() {
+	clearInterval(timerId);
+	startBtn.removeAttribute('disabled');
+	stopBtn.setAttribute('disabled', true);
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
